c940bf793fddfe6ea94fcfcaf9c7912e33980034
Justin Stewart <stewartjustin3@gmail.com>
2016-06-08 16:02:51 -0400

    added months

--- app/app.js
+++ app/app.js
@@ -10,7 +10,9 @@ angular.module('calendarDemoApp', [])
     		controller: function($scope, $element, $attrs) {
 
     		$scope.years = [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]	
+            $scope.months = ["January","February" , "March", "April", "May", "June", "July", "August", "September","October", "November", "December"]    
                 console.log('test directive');
             }
+            
           }    
         });
